734cfad646dcffa60a3851d24aeb62ff5f984ca2
LAPTOP-20HGT1GI\king <3603317@qq.com>
2020-12-14 22:30:24 +0800

    添加内联表单样式

--- src/pages/base/menus/views/Index.vue
+++ src/pages/base/menus/views/Index.vue
@@ -1,24 +1,57 @@
 <!--
  * @Author: 侯兴章 3603317@qq.com
  * @Date: 2020-11-12 21:33:17
- * @LastEditTime: 2020-12-13 22:44:22
+ * @LastEditTime: 2020-12-14 22:12:35
  * @LastEditors: 侯兴章
  * @Description:
 -->
 <template>
-  <a-row gutter="10" style="heigth: 100%">
+  <a-row gutter="10" style="heigth: 100%"  >
     <a-col :span="6">
-      <div class="panel">
-        <a-tree :show-line="true" :treeData="menuData" :showIcon="false" :replaceFields="replaceFields" :default-expanded-keys="[]" @select="onSelect"></a-tree>
+      <div class="panel content-page ">
+        <a-tree :show-line="true" :treeData="menuData" :showIcon="false"
+        :defaultExpandAll="true" :replaceFields="replaceFields"
+
+         @select="onSelect"></a-tree>
       </div>
     </a-col>
     <a-col :span="18">
-      <div class="panel">
-        <a-form layout="inline" @submit="handleSubmit">
+      <div class="panel content-page ">
+        菜单信息：
+        <a-form @submit="handleSubmit" class="inline-form">
+          <a-form-item label="上级菜单" v-bind="validateInfos.menuParentName">
+            <a-input v-model:value="formMenu.menuName" placeholder="请选择上级菜单"></a-input>
+          </a-form-item>
           <a-form-item label="菜单名称" v-bind="validateInfos.menuName">
-            <a-input v-model:value="formMenu.menuName" placeholder="菜单名称"></a-input>
+            <a-input v-model:value="formMenu.menuName" placeholder="请输入菜单名称"></a-input>
+          </a-form-item>
+          <a-form-item label="菜单编码" v-bind="validateInfos.menuCode">
+            <a-input v-model:value="formMenu.menuCode" placeholder="请输入菜单编码"></a-input>
+          </a-form-item>
+          <a-form-item label="菜单图标" v-bind="validateInfos.imageLink">
+            <a-input v-model:value="formMenu.imageLink" placeholder="请选择图标"></a-input>
+          </a-form-item>
+          <a-form-item label="路由地址" v-bind="validateInfos.htmlUrl">
+            <a-input v-model:value="formMenu.htmlUrl" placeholder="请输入路由地址" style="width: 510px"></a-input>
+          </a-form-item>
+          <a-form-item label="是否生效" v-bind="validateInfos.htmlUrl">
+            <a-input v-model:value="formMenu.htmlUrl" placeholder="请输入路由地址"></a-input>
+          </a-form-item>
+
+          <a-form-item label="是否显示" v-bind="validateInfos.htmlUrl">
+            <a-input v-model:value="formMenu.htmlUrl" placeholder="请输入路由地址"></a-input>
+          </a-form-item>
+
+          <a-form-item :wrapper-col="{span: 14, offset: 4}">
+            <a-button type="primary" @click="handleSubmit">确定</a-button>
           </a-form-item>
         </a-form>
+
+        <div class="group">
+          菜单功能：
+          <a-button type="primary">添加功能</a-button>
+          <div>菜单列表</div>
+        </div>
       </div>
     </a-col>
   </a-row>
@@ -56,12 +89,23 @@ export default defineComponent({
             required: true
           }
         ],
-        menuParentId: [
+        menuCode: [
 
+        ],
+        menuParentName: [
+          {
+            required: true
+          }
+        ],
+        htmlUrl: [
+          {
+            required: true
+          }
         ]
       }
     });
 
+    const { resetFields, validate, validateInfos } = useForm(state.formMenu, state.formRules);
     const methods = {
       getData: () => {
         ServiceGetMenus().then(res => {
@@ -72,11 +116,16 @@ export default defineComponent({
         console.log('selected', selectedKeys, info);
       },
       handleSubmit: () => {
-
+        validate()
+          .then(() => {
+            console.log(state.formMenu);
+          })
+          .catch(err => {
+            console.log('error', err);
+          });
       }
     };
 
-    const { resetFields, validate, validateInfos } = useForm(state.formMenu, state.formRules);
     onMounted(() => {
       methods.getData();
     });
@@ -85,5 +134,9 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped  >
+.group {
+  padding-top: 15px;
+  border-top: 1px solid #3c3879;
+}
 </style>
